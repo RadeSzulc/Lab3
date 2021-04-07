63ba5a054be42b32ae1d1a4b9ef2f89844762868
Radoslaw <r.szulc.762@studms.ug.edu.pl>
2021-04-07 17:13:28 +0200

    v1

--- skrypty.js
+++ skrypty.js
@@ -1,7 +1,7 @@
-const input = document.querySelector("#inputText");
+const input = document.querySelector('#formularz');
 const val = input.nodeValue;
 
-if (!val.length) {
+if (val.length) {
     console.log('Nie wpisałeś żadnej wartości!')
 } else {
     console.log('Twoje imię to ' + val[0].toLocaleUpperCase() + val.slice(1) );
