dfd0b9facfa6731f47c36d94c74d92e4a2a486bb
Radoslaw <r.szulc.762@studms.ug.edu.pl>
2021-04-09 13:23:03 +0200

    v1

--- skrypty.js
+++ skrypty.js
@@ -1,8 +1,24 @@
-const input = document.querySelector('#formularz');
-const val = input.nodeValue;
-
-if (val.length) {
-    console.log('Nie wpisałeś żadnej wartości!')
-} else {
-    console.log('Twoje imię to ' + val[0].toLocaleUpperCase() + val.slice(1) );
+function sprawdźDane(userName, userSurname, userMail) {
+    const reName = /^[a-zA-ZąĄćĆęĘłŁńŃóÓśŚźŹżŻ]{3,}$/g;
+    if(reName.test(String(userName).toLowerCase())&&/[A-Z]/.test(userName[0])){
+        alert ("Twoje imię to " + userName)
+    }
+    else {
+        alert("Imię jest niepoprawne");
+    }
+    const reSurname = /^[a-zA-ZąĄćĆęĘłŁńŃóÓśŚźŹżŻ]{3,}$/g;
+    if(reSurname.test(String(userSurname).toLowerCase())&&/[A-Z]/.test(userSurname[0])){
+        alert ("Twoje nazwisko to " + userSurname)
+    }
+    else {
+        alert("Nazwisko jest niepoprawne");
+    }
+    const reMail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(reMail.test(String(userMail).toLowerCase())){
+        alert ("Twój e-mail to " + userMail)
+    }
+    else{
+        alert("Email jest niepoprawny");
+    }
 }
+
